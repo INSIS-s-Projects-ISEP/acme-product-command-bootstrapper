92f0c1d63fbaf3b3587ab95865dfc78e7026c34f
RegianaBarbosa <regiana.cruz@acad.ifma.edu.br>
2023-04-08 15:17:42 +0100

    Initial commit

---
+++ src/main/java/com/isep/ProductCommandBootstrapper/ProductCommandBootstrapperApplication.java
@@ -0,0 +1,13 @@
+package com.isep.ProductCommandBootstrapper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductCommandBootstrapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductCommandBootstrapperApplication.class, args);
+	}
+
+}
